d7feb39091ed42070a0fc31c6e0bd75cfc432d97
keunwoo <keunwoo.kang@toss.im>
2022-12-30 20:50:46 +0900

    feat: 마커찍기

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const APP_KEY = '2d1322e7d17a2bd761063469d6dd3c97';
 
@@ -14,24 +14,51 @@ interface Props {
   lon: number;
 }
 
+// TODO(근우): DarkMode 적용
 function KakaoMap({ lat, lon }: Props) {
-  const mapRef = useRef<HTMLDivElement>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const [mapInstance, setMapInstance] = useState<any | null>(null);
 
   const initMap = useCallback(() => {
-    if (mapRef.current) {
+    if (mapContainerRef.current) {
       const { kakao } = window;
-      kakao.maps.Map(mapRef.current, {
+      const kakaoMap = new kakao.maps.Map(mapContainerRef.current, {
         center: new kakao.maps.LatLng(lat, lon),
         level: 3,
       });
+
+      setMapInstance(kakaoMap);
     }
-  }, []);
+  }, [lat, lon]);
+
+  const setMarker = useCallback(() => {
+    const { kakao } = window;
+
+    const imageSize = new kakao.maps.Size(66, 86); // 마커이미지의 크기입니다
+    const imageOption = { offset: new kakao.maps.Point(33, 86) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    const markerImage = new kakao.maps.MarkerImage('/center-marker.svg', imageSize, imageOption);
+
+    const marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(lat, lon),
+      image: markerImage,
+    });
+
+    marker.setMap(mapInstance);
+  }, [lat, lon, mapInstance]);
 
   useEffect(() => {
-    if (window?.kakao) {
+    if (window.kakao == null) {
+      return;
+    }
+
+    if (mapInstance == null) {
       initMap();
     }
-  }, [initMap]);
+
+    setMarker();
+  }, [initMap, setMarker]);
 
   return (
     <>
@@ -40,7 +67,7 @@ function KakaoMap({ lat, lon }: Props) {
         onLoad={() => window.kakao.maps.load(initMap)}
       />
       <div
-        ref={mapRef}
+        ref={mapContainerRef}
         style={{
           width: '100%',
           height: '400px',
